29eb87d5fb24bbb35ca169f0e57fe7cdffba990b
siddhikavillisaravanan <143216319+siddhikavillisaravanan@users.noreply.github.com>
2023-08-26 00:45:27 +0530

    Add files via upload
    Week1

---
+++ week1/day1/SampleJavaCode.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class SampleJavaCode {
+	//type main ctrl space
+	public static void main(String[] args) {
+		//type  syso ctrl space
+		System.out.println("Welcome to testleaf");
+	}
+
+}
